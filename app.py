f6d5fb5473926ace4f5bf1720e7040d53fe69de1
Ian_L <singlespider4@gmail.com>
2024-12-10 21:24:23 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "Heya! I’m going to crush Flask! #SkillcrushingIt."
+
+# Run the flask server
+if __name__ == "__main__":
+    app.run()
